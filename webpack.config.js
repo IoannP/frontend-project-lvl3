d23302914f815957df6c2ad70d49e36d005aca49
ioann <giohann>
2020-08-08 21:30:15 +0300

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   devtool: 'source-map',
   output: {
